4f10db18605c6f6a19f5026de3f3192dfdbb2c2f
Belljune <blackwingcrimsonblack@gmail.com>
2026-02-07 12:35:08 +0000

    로또생성기

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,36 +1,20 @@
 import streamlit as st
 
-st.title("안녕 Streamlit")
+import os, time
 
-st.header("헤더를 입력할 수 있어요 :sunglasses:")
+print(f"✅ {os.path.basename(__file__)} 실행됨 {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-st.subheader("이것은 subheader 입니다")
+import random
 
-st.text("일반적인 텍스트 출력")
+st.title(":sparkles: 로또 생성기 :sparkles:")
 
-st.caption("캡션을 넣어봅시다.")
+def generate_lotto():
+    lotto = [i + 1 for i in range(45)]
+    random.shuffle(lotto)
+    return lotto[:6]
 
-sample_code = '''
-def function():
-    print('hello, world')
-'''
+button = st.button("로또를 생성해 주세요")
 
-st.code(sample_code)
-
-st.markdown("streamlit은 **마크다운 문법**을 지원 합니다")
-
-st.table([
-    ['이름', '나이'],
-    ['홍길동', '40'],
-    ['뽀로로', '23'],
-])
-
-st.metric(label = '삼성전자', value= '151,200원', delta='1,200원')
-
-st.title('write()')
-st.write("hello")
-st.write(10, 20, 30)
-st.write([1, 2, 3, 4])
-st.write({'x':100, "y":3.14})
-
-[100, 200, 300, 400]
+if button:
+    for i in range(5):
+        st.subheader(f"{i + 1}. 행운의 번호: :green[{generate_lotto()}]")
